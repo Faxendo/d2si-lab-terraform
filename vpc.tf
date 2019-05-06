10fc7506d3d7883eb8c51a762a96ff8b7a3d193f
Nassim LAKEBIR <nassim.lakebir@d2si.io>
2019-05-06 16:56:46 +0200

    Public subnet configuration

--- vpc.tf
+++ vpc.tf
@@ -10,3 +10,44 @@ resource "aws_vpc" "vpc_terraform" {
     Project = "TP Terraform"
   }
 }
+
+########################
+#   Internet Gateway   #
+########################
+resource "aws_internet_gateway" "vpc_igw" {
+  vpc_id = "${aws_vpc.vpc_terraform.id}"
+}
+
+####################
+#   Public Subnet  #
+####################
+resource "aws_subnet" "public_1" {
+  vpc_id = "${aws_vpc.vpc_terraform.id}"
+
+  cidr_block        = "${var.public_subnet}"
+  availability_zone = "${var.aws_region_az_1}"
+
+  tags = {
+    Name    = "Public Subnet 1"
+    Project = "TP Terraform"
+  }
+}
+
+resource "aws_route_table" "az_1_public" {
+  vpc_id = "${aws_vpc.vpc_terraform.id}"
+
+  route = {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = "${aws_internet_gateway.vpc_igw.id}"
+  }
+
+  tags = {
+    Name    = "Public Subnet Route Table"
+    Project = "TP Terraform"
+  }
+}
+
+resource "aws_route_table_association" "az_1_public" {
+  route_table_id = "${aws_route_table.az_1_public.id}"
+  subnet_id      = "${aws_subnet.public_1.id}"
+}
