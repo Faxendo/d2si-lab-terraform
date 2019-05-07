55f1323653658573b6da60002ce106510019b284
Nassim LAKEBIR <nassim.lakebir@d2si.io>
2019-05-07 15:49:47 +0200

    Terraform fmt

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
-    backend "s3" {
-        bucket = "d2si-tp-terraform-shauni"
-        key = "tp-1/terraform.tfstate"
-        region = "eu-west-1"       
-    }
+  backend "s3" {
+    bucket = "d2si-tp-terraform-shauni"
+    key    = "tp-1/terraform.tfstate"
+    region = "eu-west-1"
+  }
 }
